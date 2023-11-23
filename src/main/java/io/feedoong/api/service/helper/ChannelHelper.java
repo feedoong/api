2b41bac6cefd070aff8998bed753f7dc099d8525
HoJun Jang <bigfanoftim@gmail.com>
2023-11-23 19:15:15 +0900

    refactor: helper가 아닌 mapper에서 DTO 생성하도록 변경

--- src/main/java/io/feedoong/api/service/helper/ChannelHelper.java
+++ src/main/java/io/feedoong/api/service/helper/ChannelHelper.java
@@ -3,34 +3,17 @@ package io.feedoong.api.service.helper;
 import io.feedoong.api.domain.Channel;
 import io.feedoong.api.domain.User;
 import io.feedoong.api.domain.repository.ChannelRepository;
-import io.feedoong.api.service.dto.ChannelDetailsDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
 public class ChannelHelper {
     private final ChannelRepository channelRepository;
 
-    public Page<ChannelDetailsDTO> getSubscribedChannels(Pageable pageable, User user) {
-        Page<Channel> subscribedChannels = channelRepository.findSubscribedChannelsByUser(pageable, user);
-        List<ChannelDetailsDTO> channelDetailsDTOList = subscribedChannels.stream()
-                .map(channel -> ChannelDetailsDTO.builder()
-                        .id(channel.getId())
-                        .title(channel.getTitle())
-                        .description(channel.getDescription())
-                        .url(channel.getUrl())
-                        .feedUrl(channel.getFeedUrl())
-                        .imageUrl(channel.getImageUrl())
-                        .isSubscribed(true)
-                        .build())
-                .collect(Collectors.toList());
-        return new PageImpl<>(channelDetailsDTOList, pageable, subscribedChannels.getTotalElements());
+    public Page<Channel> getSubscribedChannels(Pageable pageable, User user) {
+        return channelRepository.findSubscribedChannelsByUser(pageable, user);
     }
 }
