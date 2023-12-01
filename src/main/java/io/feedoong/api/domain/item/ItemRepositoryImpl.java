da83356214d0f93f07898713e10539ebce68c48e
HoJun Jang <bigfanoftim@gmail.com>
2023-12-01 16:30:07 +0900

    fix: count query에서 페이징 제거

--- src/main/java/io/feedoong/api/domain/item/ItemRepositoryImpl.java
+++ src/main/java/io/feedoong/api/domain/item/ItemRepositoryImpl.java
@@ -1,13 +1,11 @@
 package io.feedoong.api.domain.item;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import io.feedoong.api.domain.user.User;
 import io.feedoong.api.domain.dto.ChannelItemDTO;
+import io.feedoong.api.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +13,8 @@ import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
 
+import static com.querydsl.core.types.Projections.constructor;
+import static com.querydsl.core.types.dsl.Expressions.asBoolean;
 import static io.feedoong.api.domain.channel.QChannel.channel;
 import static io.feedoong.api.domain.item.QItem.item;
 import static io.feedoong.api.domain.like.QLike.like;
@@ -27,17 +27,8 @@ public class ItemRepositoryImpl implements CustomItemRepository {
 
     @Override
     public Page<ChannelItemDTO> findChannelItemByUser(Pageable pageable, User user) {
-        BooleanExpression isLiked = JPAExpressions
-                .selectFrom(like)
-                .where(like.user.eq(user), like.item.eq(item))
-                .exists();
-        BooleanExpression isViewed = JPAExpressions
-                .selectFrom(view)
-                .where(view.user.eq(user), view.item.eq(item))
-                .exists();
-
         List<ChannelItemDTO> contents = queryFactory
-                .select(Projections.constructor(ChannelItemDTO.class,
+                .select(constructor(ChannelItemDTO.class,
                         item.id,
                         item.title,
                         item.description,
@@ -45,8 +36,8 @@ public class ItemRepositoryImpl implements CustomItemRepository {
                         item.guid,
                         item.publishedAt,
                         item.imageUrl,
-                        Expressions.asBoolean(isLiked).as("isLiked"),
-                        Expressions.asBoolean(isViewed).as("isViewed"),
+                        asBoolean(isLikedExp(user)).as("isLiked"),
+                        asBoolean(isViewedExp(user)).as("isViewed"),
                         channel.id,
                         channel.title,
                         channel.imageUrl))
@@ -59,10 +50,22 @@ public class ItemRepositoryImpl implements CustomItemRepository {
         JPAQuery<Item> countQuery = queryFactory
                 .selectFrom(item)
                 .join(item.channel, channel)
-                .join(subscription).on(subscription.channel.eq(channel).and(subscription.user.eq(user)))
-                .limit(pageable.getPageSize())
-                .offset(pageable.getOffset());
+                .join(subscription).on(subscription.channel.eq(channel).and(subscription.user.eq(user)));
 
         return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchCount);
+    }
+
+    private static BooleanExpression isLikedExp(User user) {
+        return JPAExpressions
+                .selectFrom(like)
+                .where(like.user.eq(user), like.item.eq(item))
+                .exists();
+    }
+
+    private static BooleanExpression isViewedExp(User user) {
+        return JPAExpressions
+                .selectFrom(view)
+                .where(view.user.eq(user), view.item.eq(item))
+                .exists();
     }
 }
