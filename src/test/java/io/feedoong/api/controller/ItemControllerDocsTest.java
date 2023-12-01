c7b6428f2028d5dedcc30e932292fda9792d5731
HoJun Jang <bigfanoftim@gmail.com>
2023-12-01 19:28:33 +0900

    docs: 특정 채널의 아이템 조회 기능에 대한 REST Docs 추가

--- src/test/java/io/feedoong/api/controller/ItemControllerDocsTest.java
+++ src/test/java/io/feedoong/api/controller/ItemControllerDocsTest.java
@@ -137,4 +137,77 @@ class ItemControllerDocsTest {
                     ));
         }
     }
+
+    @Nested
+    @DisplayName("GET /v2/items/channel/{channelId} - getChannelItems 메소드")
+    class GetChannelItems {
+        private String token;
+        private Channel channel;
+
+        @BeforeEach
+        void prepare() {
+            User user = UserFactory.create();
+            userRepository.save(user);
+            token = tokenProvider.create(user);
+
+            channel = ChannelFactory.create();
+            channelRepository.save(channel);
+
+            Item item = ItemFactory.create(channel);
+            itemRepository.save(item);
+        }
+
+        @Test
+        @DisplayName("로그인 사용자의 아이템 조회 성공")
+        public void getChannelItems_Success() throws Exception {
+            mockMvc.perform(get("/v2/items/channel/{channelId}", channel.getId())
+                            .header("Authorization", token)
+                            .param("page", "0")
+                            .param("size", "10")
+                            .param("sort", "publishedAt")
+                            .param("direction", "desc"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.totalPages").value(1))
+                    .andExpect(jsonPath("$.totalElements").value(1))
+                    .andExpect(jsonPath("$.page").value(0))
+                    .andExpect(jsonPath("$.size").value(10))
+                    .andExpect(jsonPath("$.hasNext").value(false))
+                    .andExpect(jsonPath("$.isFirst").value(true))
+                    .andExpect(jsonPath("$.isLast").value(true))
+                    .andExpect(jsonPath("$.contents").isArray())
+                    .andDo(document("v2/items/channel/channelId/login",
+                            fromRequest(),
+                            fromResponse(),
+                            RequestDocumentation.queryParameters(
+                                    parameterWithName("page").description("페이지 번호 (0부터 시작)"),
+                                    parameterWithName("size").description("한 페이지 당 반환되는 항목의 수"),
+                                    parameterWithName("sort").description("정렬 기준이 되는 필드 (예: 'publishedAt')"),
+                                    parameterWithName("direction").description("정렬 방향 (asc 또는 desc)")
+                            ),
+                            PayloadDocumentation.responseFields(
+                                    fieldWithPath("totalPages").type(JsonFieldType.NUMBER).description("총 페이지 수"),
+                                    fieldWithPath("totalElements").type(JsonFieldType.NUMBER).description("총 요소 수"),
+                                    fieldWithPath("page").type(JsonFieldType.NUMBER).description("현재 페이지 번호"),
+                                    fieldWithPath("size").type(JsonFieldType.NUMBER).description("페이지 당 항목 수"),
+                                    fieldWithPath("hasNext").type(JsonFieldType.BOOLEAN).description("다음 페이지 존재 여부"),
+                                    fieldWithPath("isFirst").type(JsonFieldType.BOOLEAN).description("현재 페이지가 첫 번째 페이지인지 여부"),
+                                    fieldWithPath("isLast").type(JsonFieldType.BOOLEAN).description("현재 페이지가 마지막 페이지인지 여부"),
+                                    fieldWithPath("contents").type(JsonFieldType.ARRAY).description("현재 페이지의 콘텐츠 목록 (ChannelItemDTO)"),
+                                    fieldWithPath("contents[].id").type(JsonFieldType.NUMBER).description("아이템 ID"),
+                                    fieldWithPath("contents[].title").type(JsonFieldType.STRING).description("아이템 제목"),
+                                    fieldWithPath("contents[].description").type(JsonFieldType.STRING).description("아이템 설명"),
+                                    fieldWithPath("contents[].link").type(JsonFieldType.STRING).description("아이템 URL"),
+                                    fieldWithPath("contents[].guid").type(JsonFieldType.STRING).description("아이템 GUID"),
+                                    fieldWithPath("contents[].publishedAt").type(JsonFieldType.STRING).description("아이템 퍼블리싱 시점"),
+                                    fieldWithPath("contents[].imageUrl").type(JsonFieldType.STRING).description("아이템 이미지 URL"),
+                                    fieldWithPath("contents[].isLiked").type(JsonFieldType.BOOLEAN).description("좋아요 여부, 로그인하지 않은 사용자의 경우 'false' 로 고정"),
+                                    fieldWithPath("contents[].isViewed").type(JsonFieldType.BOOLEAN).description("조회 여부, 로그인하지 않은 사용자의 경우 'false'로 고정"),
+                                    fieldWithPath("contents[].channelId").type(JsonFieldType.NUMBER).description("채널 ID"),
+                                    fieldWithPath("contents[].channelTitle").type(JsonFieldType.STRING).description("채널 제목"),
+                                    fieldWithPath("contents[].channelImageUrl").type(JsonFieldType.STRING).description("채널 이미지 URL")
+                            )
+                    ));
+        }
+    }
 }
