96e299cb5ca86234e87f777707be5b475b154bf8
HoJun Jang <bigfanoftim@gmail.com>
2023-12-02 15:10:50 +0900

    refactor: ChannelControllerRestDocsTest 개선
    - @SpringBootTest 대신 @WebMvcTest로 변경하여 불필요한 컨텍스트 로딩 방지
    - 테스트가 변경을 감지하고 실패를 유도할 수 있도록 검증 로직을 추가

--- src/test/java/io/feedoong/api/restdocs/ChannelControllerRestDocsTest.java
+++ src/test/java/io/feedoong/api/restdocs/ChannelControllerRestDocsTest.java
@@ -1,70 +1,76 @@
 package io.feedoong.api.restdocs;
 
-import io.feedoong.api.domain.channel.Channel;
-import io.feedoong.api.domain.channel.ChannelRepository;
-import io.feedoong.api.domain.user.User;
-import io.feedoong.api.domain.user.UserRepository;
-import io.feedoong.api.shared.base.BaseRestDocsTest;
-import io.feedoong.api.shared.factory.ChannelFactory;
-import io.feedoong.api.shared.factory.UserFactory;
-import org.junit.jupiter.api.BeforeEach;
+import io.feedoong.api.controller.ChannelController;
+import io.feedoong.api.domain.channel.dto.ChannelDetailsDTO;
+import io.feedoong.api.service.ChannelService;
+import io.feedoong.api.shared.mock.ChannelMock;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static io.feedoong.api.shared.util.ApiDocumentationUtils.fromRequest;
 import static io.feedoong.api.shared.util.ApiDocumentationUtils.fromResponse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@AutoConfigureRestDocs
+@EnableWebSecurity
+@WebMvcTest(ChannelController.class)
+@ActiveProfiles("test")
 @DisplayName("ChannelController REST Docs")
-class ChannelControllerRestDocsTest extends BaseRestDocsTest {
+class ChannelControllerRestDocsTest {
     @Autowired
-    private UserRepository userRepository;
+    private MockMvc mockMvc;
 
-    @Autowired
-    private ChannelRepository channelRepository;
+    @MockBean
+    private ChannelService channelService;
 
-    @Nested
-    @DisplayName("GET /v2/channels/{channelId} - getChannelDetails 메소드")
-    class GetChannelDetails {
-        private String token;
-        private Channel channel;
+    @Test
+    @WithMockUser
+    @DisplayName("GET /v2/channels/{channelId} -  getChannelDetails")
+    public void getChannelDetails() throws Exception {
+        ChannelDetailsDTO channelDetails = ChannelMock.channelDetailsDTO();
+        when(channelService.getChannelDetails(anyLong(), any(UserDetails.class)))
+                .thenReturn(channelDetails);
 
-        @BeforeEach
-        void prepare() {
-            User user = UserFactory.create();
-            userRepository.save(user);
-            token = tokenProvider.create(user);
-
-            channel = ChannelFactory.create();
-            channelRepository.save(channel);
-        }
-
-        @Test
-        public void success() throws Exception {
-            mockMvc.perform(get("/v2/channels/{channelId}", channel.getId())
-                            .header("Authorization", token))
-                    .andDo(print())
-                    .andDo(document("v2/channels/channelId",
-                            fromRequest(),
-                            fromResponse(),
-                            PayloadDocumentation.responseFields(
-                                    fieldWithPath("data").type(JsonFieldType.OBJECT).description("채널 정보 (ChannelDetailsDTO)"),
-                                    fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("채널 ID"),
-                                    fieldWithPath("data.title").type(JsonFieldType.STRING).description("채널 제목"),
-                                    fieldWithPath("data.description").type(JsonFieldType.STRING).description("채널 설명"),
-                                    fieldWithPath("data.url").type(JsonFieldType.STRING).description("채널 URL"),
-                                    fieldWithPath("data.feedUrl").type(JsonFieldType.STRING).description("채널 피드 URL"),
-                                    fieldWithPath("data.imageUrl").type(JsonFieldType.STRING).description("채널 이미지 URL"),
-                                    fieldWithPath("data.isSubscribed").type(JsonFieldType.BOOLEAN).description("구독 여부, 로그인하지 않은 사용자의 경우 'false'로 고정")
-                            )
-                    ));
-        }
+        mockMvc.perform(get("/v2/channels/{channelId}", channelDetails.getId()))
+                .andDo(print())
+                .andExpect(jsonPath("$.data.id").value(channelDetails.getId()))
+                .andExpect(jsonPath("$.data.title").value(channelDetails.getTitle()))
+                .andExpect(jsonPath("$.data.description").value(channelDetails.getDescription()))
+                .andExpect(jsonPath("$.data.url").value(channelDetails.getUrl()))
+                .andExpect(jsonPath("$.data.feedUrl").value(channelDetails.getFeedUrl()))
+                .andExpect(jsonPath("$.data.imageUrl").value(channelDetails.getImageUrl()))
+                .andExpect(jsonPath("$.data.isSubscribed").value(channelDetails.getIsSubscribed()))
+                .andDo(document("v2/channels/channelId",
+                        fromRequest(),
+                        fromResponse(),
+                        PayloadDocumentation.responseFields(
+                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("채널 정보 (ChannelDetailsDTO)"),
+                                fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("채널 ID"),
+                                fieldWithPath("data.title").type(JsonFieldType.STRING).description("채널 제목"),
+                                fieldWithPath("data.description").type(JsonFieldType.STRING).description("채널 설명"),
+                                fieldWithPath("data.url").type(JsonFieldType.STRING).description("채널 URL"),
+                                fieldWithPath("data.feedUrl").type(JsonFieldType.STRING).description("채널 피드 URL"),
+                                fieldWithPath("data.imageUrl").type(JsonFieldType.STRING).description("채널 이미지 URL"),
+                                fieldWithPath("data.isSubscribed").type(JsonFieldType.BOOLEAN).description("구독 여부, 로그인하지 않은 사용자의 경우 'false'로 고정")
+                        )
+                ));
     }
 }
