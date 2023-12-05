8a8f4bd4c663053d3373d194557cf1e22d71154d
HoJun Jang <bigfanoftim@gmail.com>
2023-12-05 22:57:12 +0900

    refactor: ApiApplication main 메소드 테스트에서 @SpringBootTest 어노테이션 제거

--- src/test/java/io/feedoong/api/ApiApplicationTests.java
+++ src/test/java/io/feedoong/api/ApiApplicationTests.java
@@ -1,15 +1,25 @@
 package io.feedoong.api;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.mockito.ArgumentMatchers.any;
+
 @ActiveProfiles("test")
-@SpringBootTest
+@DisplayName("ApiApplication 클래스")
 class ApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    @DisplayName("main 메소드 테스트")
+    void apiApplicationTest() {
+        try (MockedStatic<SpringApplication> mocked = Mockito.mockStatic(SpringApplication.class)) {
+            mocked.when(() -> SpringApplication.run((Class<?>) any(), any())).thenReturn(null);
+            ApiApplication.main(new String[]{});
+            mocked.verify(() -> SpringApplication.run(ApiApplication.class, new String[]{}));
+        }
     }
-
 }
